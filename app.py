39447b1bea122ff87ccf395b108be8a89771353b
Mega-Barrel <saurabhjoshi24business@gmail.com>
2021-02-07 17:38:36 +0530

    Added link to 3rd page

--- app.py
+++ app.py
@@ -1,13 +1,14 @@
 
 import streamlit as st
 from multiapp import MultiApp
-from apps import home, data_stats # import your app modules here
+from apps import home, data_stats, recruiting # import your app modules here
 
 app = MultiApp()
 
 # Add all your application here
 app.add_app("Home", home.app)
 app.add_app("Data Stats", data_stats.app)
+app.add_app("Recruiting Stats", recruiting.app)
 
 # The main app
 app.run()
