291328fc10c49d2c897fefa0586b16b2ebef4db2
Mega-Barrel <saurabhjoshi24business@gmail.com>
2021-02-07 17:38:48 +0530

    Added Viz.

--- apps/data_stats.py
+++ apps/data_stats.py
@@ -1,15 +1,57 @@
 import streamlit as st
 import numpy as np
 import pandas as pd
-from data.create_data import create_table
+import matplotlib.pyplot as plt
+from plotly import graph_objs as go
+from apps.home import get_data
+import plotly.express as px
+import plotly.graph_objects as go
+
+
 
 def app():
-    st.title('Data Stats')
+    st.title('Data Visualization')
 
-    st.write("This is a sample data stats in the mutliapp.")
-    st.write("See `apps/data_stats.py` to know how to use it.")
+    # st.write("This is a sample data stats in the mutliapp.")
+    # st.write("See `apps/data_stats.py` to know how to use it.")
 
-    st.markdown("### Plot Data")
-    df = create_table()
+    data = get_data('datasets/currenttwit.csv')
 
-    st.line_chart(df)
+
+    # Total Count of Players Rating as per state
+    fig = go.Figure()
+    for name, group in data.groupby('state'):
+        trace = go.Histogram()
+        trace.name = name
+        trace.x = group['RATING']
+        fig.add_trace(trace)
+
+    fig.update_layout(
+        title_text = 'Total counts of Players Rating as per State',
+        width=900,
+        height=500
+    )
+    st.plotly_chart(fig)
+
+
+    # Map View
+    fig = go.Figure(data=go.Choropleth(
+        locations = data['state'], # Spatial coordinates
+        z = data['sentiment'].astype(float), # Data to be color-coded
+        locationmode = 'USA-states', # set of locations match entries in `locations`
+        colorscale = 'Magenta',
+        colorbar_title = "Sentiment As per States",
+    ))
+
+    fig.update_layout(
+        title_text = 'Football player Sentiment Values by State',
+        geo_scope='usa', # limite map scope to USA
+    )
+
+    st.plotly_chart(fig)
+
+
+
+
+    
+    
